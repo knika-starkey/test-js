59ec6902ae3fe25781cac800566253d99c6fd604
knika-starkey <knika787@gmail.com>
2022-11-26 23:14:55 +0200

    pareStr LowerCase

--- pareStr/script.js
+++ pareStr/script.js
@@ -1,7 +1,7 @@
 let lines = [
   "I met her in a club down in old Soho",
   "Where you drink champagne and it tastes just like Coca-Cola",
-  "C-O-L-A, Cola",
+  "c-O-L-A, Cola",
   "She walked up to me and she asked me to dance",
   `I asked her her name and in a dark brown voice she said, "Lola"`,
   "L-O-L-A, Lola",
@@ -12,6 +12,8 @@ for (let i = 0; i < lines.length; i++) {
   if (i % 2 == 0) pareLines.push(lines[i]);
 }
 let compare = (colaA, colaB) => {
+  colaA = colaA.toLowerCase();
+  colaB = colaB.toLowerCase();
   return colaA[0] > colaB[0] ? 1 : colaA[0] === colaB[0] ? 0 : -1;
 };
 pareLines.sort(compare);
